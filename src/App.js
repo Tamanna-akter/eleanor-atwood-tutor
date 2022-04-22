67d98d54ae1d5cb2331d727e54a0ec6dae8cc3a2
Tamanna-akter <atamanna577@gmail.com>
2022-04-23 01:14:46 +0600

    about section done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
 import Blogs from './Pages/Blogs/Blogs';
 import Home from './Pages/Home/Home/Home';
 import Services from './Pages/Home/Services/Services';
@@ -18,6 +19,7 @@ function App() {
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
         <Route path="/blogs" element={<Blogs> </Blogs>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
         <Route path='/service/:serviceId' element={<ServiceDetails></ServiceDetails>}></Route>
 
       </Routes>
--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import myPic from './../../images/my_pic.png';
+import './About.css';
 
 const About = () => {
     return (
         <div>
-            <h3>hi</h3>
+            <h2 className='text-center text-primary fw-bold mt-4'>About me</h2>
+            <div className='d-flex flex-row mx-5 my-4 '>
+
+                <div>
+                    <img className='myPic' src={myPic} alt="" />
+                </div>
+                <div className='my-info' >
+                    <h5 className='text-primary fw-bold'> I'am Tamanna Akter</h5>
+                    <p className='fst-italic fs-5 text-alignment-justify'>I am a front-end web developer.My next plan is to become a full stack web developer and want to gain experties on rectjs,nodejs.To become a Web Developer, i should have an understanding of HTML, CSS, and JavaScript. Its also recommended to learn about CSS and CSS frameworks.I have to work hard to make this goal successful. </p>
+                </div>
+            </div>
         </div>
     );
 };
